36e0ef23b6576f1375bd0ff4eb345265c4f2d7cb
Bebebole <79281901+Bebebole@users.noreply.github.com>
2022-01-12 17:03:26 +0000

    Add files via upload

--- Terminal/Bebebole.py
+++ Terminal/Bebebole.py
@@ -1,63 +1,65 @@
 import os
 import sys
-from pathlib import Path
-import requests
-import configparser
-import shutil
-import re
-try:
-	from googlesearch import search
-except ImportError:
-	print("Tu dois installer google avec pip ! (pip install google)")
-
 
 x = [f.name for f in os.scandir() if f.is_file()]
 
-os.chdir("C:\Program Files")
+import pathlib
+from pathlib import Path
+def get_datadir() -> pathlib.Path:
+
+    home = pathlib.Path.home()
+
+    if sys.platform == "win32":
+        return home / "AppData/Roaming"
+    elif sys.platform == "linux":
+        return home / ".local/share"
+    elif sys.platform == "darwin":
+        return home / "Library/Application Support"
+
+my_datadir = get_datadir() / "BebeboleIconPackMaker"
 
 try:
-    os.mkdir("Bebebole")
+    my_datadir.mkdir(parents=True)
 except FileExistsError:
     pass
 
-os.chdir("C:\Program Files\Bebebole")
+os.chdir(my_datadir)
 
 try:
-    os.mkdir("IconPackMaker")
+    os.mkdir("temp")
 except FileExistsError:
     pass
 
-os.chdir("C:\Program Files\Bebebole\IconPackMaker")
-
+import webbrowser
+import requests
 try :
     open("pathsettings.ini","x")
     file = open("pathsettings.ini","w")
     response = requests.get('https://raw.githubusercontent.com/Bebebole/BebeboleIconsPython/main/Terminal/Resources/pathsettings.ini')
     file.write(response.text)
     file.close()
-    print("Tu dois mettre à jour le fichier pour que le programme marche correctement !\nIl faut modifier le fichier 'pathsettings.ini'(C:\Program Files\Bebebole\IconPackMaker)")
+    print("Tu dois mettre à jour le fichier pour que le programme marche correctement !\nIl faut modifier le fichier 'pathsettings.ini'")
+    webbrowser.open("pathsettings.ini")
     input()
-
 except FileExistsError:
     pass
 
 try :
     response = requests.get('https://raw.githubusercontent.com/Bebebole/BebeboleIconsPython/main/Terminal/Resources/ComponentInfo_db.txt')
     open('ComponentInfo_db.txt', 'wb').write(response.content)
-
 except FileExistsError:
     pass
 
+import configparser
 config = configparser.ConfigParser()
 config.read("pathsettings.ini")
 icon_temp = config.get("path", "icon_temp")
 icon_backup = config.get("path", "icon_backup")
 icon_all = config.get("path", "icon_all")
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
 print("\n \033[1;32;40m██████╗░███████╗██████╗░███████╗██████╗░░█████╗░██╗░░░░░███████╗\n ██╔══██╗██╔════╝██╔══██╗██╔════╝██╔══██╗██╔══██╗██║░░░░░██╔════╝\n ██████╦╝█████╗░░██████╦╝█████╗░░██████╦╝██║░░██║██║░░░░░█████╗░░\n ██╔══██╗██╔══╝░░██╔══██╗██╔══╝░░██╔══██╗██║░░██║██║░░░░░██╔══╝░░\n ██████╦╝███████╗██████╦╝███████╗██████╦╝╚█████╔╝███████╗███████╗\n ╚═════╝░╚══════╝╚═════╝░╚══════╝╚═════╝░░╚════╝░╚══════╝╚══════╝\n")
-clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 question2 = input("\n\n\033[34;1;40mSalut ! \033[37;1;40mTu veux faire quoi ?\n\n[a] Remplir le fichier \033[31;1;40mappfilter\n\033[37;1;40m[b] Remplir le fichier \033[31;1;40mdrawable\n\033[37;1;40m[c] Remplir le fichier \033[31;1;40micon_pack\n\033[37;1;40m[d] Trier les icônes\n\033[37;1;40m[e] Trouver le ComponentInfo d'une App\n\n\033[33;1;40mAlors ? : ")
-
 
 #Appfilter
 if question2 == "a":
@@ -94,6 +96,7 @@ if question2 == "c":
 
 #Trier
 if question2 == "d":
+    import shutil
     clearConsole()
     src_files = os.listdir(icon_temp)
     for file_name in src_files:
@@ -115,29 +118,24 @@ if question2 == "d":
 if question2 == "e":
     clearConsole()
     appsearch= input("Le nom de l'app : ")
-    searchongoogle1 = "m.apkpure.com" + appsearch
+    searchongoogle = "m.apkpure.com" + appsearch
     linkapk = "https://m.apkpure.com/*/"
 
+    from googlesearch import search
+    for respo in search(searchongoogle, tld="co.in", num=10, stop=1, pause=2):
+        appid = respo.lstrip(linkapk)
 
-    for respo in search(searchongoogle1, tld="co.in", num=10, stop=1, pause=2):
-        appid1 = respo.lstrip(linkapk)
-
-    appid2 = re.sub(r'^.*?/', '/', appid1)
-    appid3 = appid2.lstrip("/")
-
+    import re
+    appid = re.sub(r'^.*?/', '/', appid)
+    appid = appid.lstrip("/")
     clearConsole()
-    print(appsearch + " : " + appid3 + "\n")
-
-    questionsearchdbci = input("Tu veux chercher dans la db ? (o/n) : ")
-
-    if questionsearchdbci == "o":
-        clearConsole()
-        os.chdir("C:\Program Files\Bebebole\IconPackMaker")
-        with open("ComponentInfo_db.txt") as openfile:
-            for line in openfile:
-                if appid3 in line:
-                    print(line + "\n\n" + appsearch + " : " + appid3)
-    if questionsearchdbci == "n":
-            pass
-
+    os.chdir(my_datadir)
+    print("\033[32;1;40mAppId : \033[33;1;40m" + appid + "\n\n\033[32;1;40mApp Name : \033[33;1;40m" + appsearch)
+    with open("ComponentInfo_db.txt") as openfile:
+        for line in openfile:
+            if appid in line:
+                line = re.match(r'^.*?\"', line).group(0)
+                line = line.replace(' "',"")
+                print("\n\033[32;1;40mComponentInfo :\033[33;1;40m" + line)
+    
 input()
